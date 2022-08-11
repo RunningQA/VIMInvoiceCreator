8d2c8c6a40b4bdf8d00e0de29d322ddaf2651c7f
jeandelisseen <jean@running-qa.com>
2022-08-11 18:35:08 +0200

    Setup.py + tests created

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='VimInvoiceCreator',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Jean Delissen',
+    author_email='jean@running-qa.com',
+    description='Tool create PDF Invoices for SAP VIM'
+)
