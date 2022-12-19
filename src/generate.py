52f64a3568eea7be31906a5f775629eaf78ab0ad
jeandelisseen <jean@running-qa.com>
2022-12-19 10:58:53 +0100

    Latest changes added

--- src/generate.py
+++ src/generate.py
@@ -4,7 +4,6 @@ The Generate file takes input from the GUI and creates the PDF.
 
 # import python libraries
 import os
-import datetime
 from InvoiceGenerator.api import Invoice, Item, Client, Provider, Creator
 from InvoiceGenerator.pdf import SimpleInvoice, CorrectingInvoice
 
@@ -13,9 +12,9 @@ from InvoiceGenerator.pdf import SimpleInvoice, CorrectingInvoice
 def generate_invoice(client_name, client_address, client_zipcode, client_city, client_country, client_vatid,
                     supplier_name, supplier_address, supplier_zipcode, supplier_city, supplier_country,
                     supplier_bank_account, supplier_vatid, number_of_items, price, product_description, tax_perc, number, po_number,
-                    invoice_creator, filename, filefolder, invoice_type):
+                    coding_string, invoice_creator, filename, filefolder, invoice_type, invoice_date, currency, use_tax, original_invoice,
+                     reason):
     """function to generate pdf invoice"""
-    today = datetime.date.today()
     filestring = filefolder + '/' + filename + '.PDF'
     os.environ["INVOICE_LANG"] = "nl"
     client = Client(client_name, address=client_address, zip_code=client_zipcode, city=client_city,
@@ -24,21 +23,25 @@ def generate_invoice(client_name, client_address, client_zipcode, client_city, c
                         country=supplier_country, bank_account=supplier_bank_account, vat_id=supplier_vatid)
     creator = Creator(invoice_creator)
     invoice = Invoice(client, provider, creator)
-    #invoice.add_item(Item(count=100, price="2.65", description="Steunprofiel, steek 50", tax="0"))
+    #invoice.add_item(Item(count=1, price="23.87", description="Pickwick Thee top 10", tax="21"))
     #invoice.add_item(Item(count=20, price="8.43", description="Kruimellade tbv brobankrat", tax="12"))
     invoice.add_item(Item(count=number_of_items, price=price, description=product_description, tax=tax_perc))
-    invoice.currency = "€"
+    invoice.currency = currency
     invoice.number = number
     invoice.purchase_order = po_number
-    invoice.taxable_date = today
-    invoice.date = today
+    invoice.coding_string = coding_string
+    invoice.taxable_date = invoice_date
+    invoice.date = invoice_date
     invoice.generate_breakdown_vat()
-    invoice.use_tax = True
+    invoice.use_tax = use_tax
+
+    # condition to determine invoice type
     if invoice_type == 'Normal':
         docu = SimpleInvoice(invoice)
     elif invoice_type == 'Credit':
         docu = CorrectingInvoice(invoice)
-        invoice.original = "INVEP359"
-        invoice.reason = "Artikelen teruggestuurd"
+        invoice.original = original_invoice
+        invoice.reason = reason
+
     docu.gen(filestring)
     #docu.gen(filestring, generate_qr_code=False)  # you can put QR code by setting the #qr_code parameter to ‘True’
--- src/gui.py
+++ src/gui.py
@@ -4,6 +4,7 @@ The GUI file creates the GUI and sends the input values to generate.py.
 #import necessary packages and python functions
 
 import PySimpleGUI as sg
+import datetime
 from generate import generate_invoice
 
 # GUI
@@ -11,35 +12,44 @@ layout = [
     [sg.Radio('Normal Invoice', 1, enable_events=True, default=True, key='-NORMAL_INVOICE-'), sg.Radio('Credit Nota',1, enable_events=True, key='-CREDIT_NOTA-')],
     [sg.Text('Please enter all necessary invoice data:')],
     [sg.Text(' ')],
-    [sg.Text('KLANTGEGEVENS')],
-    [sg.Text('Naam Klant', size =(15, 1)), sg.Input(key='-CLIENT_NAME-')],
-    [sg.Text('Adres', size =(15, 1)), sg.Input(key='-CLIENT_ADDRESS-')],
-    [sg.Text('Postcode', size =(15, 1)), sg.Input(key='-CLIENT_ZIPCODE-')],
-    [sg.Text('Plaats', size =(15, 1)), sg.Input(key='-CLIENT_CITY-')],
-    [sg.Text('Land', size =(15, 1)), sg.Input(key='-CLIENT_COUNTRY-')],
-    [sg.Text('BTW Nummer', size =(15, 1)), sg.Input(key='-CLIENT_VATID-')],
+    [sg.Text('CUSTOMER DETAILS')],
+    [sg.Text('Client name', size =(15, 1)), sg.Input(key='-CLIENT_NAME-')],
+    [sg.Text('Address', size =(15, 1)), sg.Input(key='-CLIENT_ADDRESS-')],
+    [sg.Text('Zip code', size =(15, 1)), sg.Input(key='-CLIENT_ZIPCODE-')],
+    [sg.Text('City', size =(15, 1)), sg.Input(key='-CLIENT_CITY-')],
+    [sg.Text('Country', size =(15, 1)), sg.Input(key='-CLIENT_COUNTRY-')],
+    [sg.Text('VAT ID (BTW)', size =(15, 1)), sg.Input(key='-CLIENT_VATID-')],
     [sg.Text(' ')],
-    [sg.Text('LEVERANCIER GEGEVENS')],
-    [sg.Text('Naam Leverancier', size =(15, 1)), sg.Input(key='-SUPPLIER_NAME-')],
-    [sg.Text('Adres', size=(15, 1)), sg.Input(key='-SUPPLIER_ADDRESS-')],
-    [sg.Text('Postcode', size=(15, 1)), sg.Input(key='-SUPPLIER_ZIPCODE-')],
-    [sg.Text('Plaats', size=(15, 1)), sg.Input(key='-SUPPLIER_CITY-')],
-    [sg.Text('Land', size=(15, 1)), sg.Input(key='-SUPPLIER_COUNTRY-')],
-    [sg.Text('BTW Nummer', size=(15, 1)), sg.Input(key='-SUPPLIER_VATID-')],
+    [sg.Text('SUPPLIER DETAILS')],
+    [sg.Text('Supplier name', size =(15, 1)), sg.Input(key='-SUPPLIER_NAME-')],
+    [sg.Text('Address', size=(15, 1)), sg.Input(key='-SUPPLIER_ADDRESS-')],
+    [sg.Text('Zip code', size=(15, 1)), sg.Input(key='-SUPPLIER_ZIPCODE-')],
+    [sg.Text('City', size=(15, 1)), sg.Input(key='-SUPPLIER_CITY-')],
+    [sg.Text('Country', size=(15, 1)), sg.Input(key='-SUPPLIER_COUNTRY-')],
+    [sg.Text('VAT ID (BTW)', size=(15, 1)), sg.Input(key='-SUPPLIER_VATID-')],
     [sg.Text('IBAN', size=(15, 1)), sg.Input(key='-SUPPLIER_BANKACCOUNT-')],
     [sg.Text(' ')],
-    [sg.Text('FACTUUR HEADER GEGEVENS')],
-    [sg.Text('Factuurnummer', size =(15, 1)), sg.Input(key='-INVOICE_NUMBER-', do_not_clear=False)],
-    [sg.Text('PO nummer', size=(15, 1)), sg.Input(key='-PO-', do_not_clear=False)],
-    [sg.Text('Aangemaakt door', size=(15, 1)), sg.Input(key='-CREATOR-')],
+    [sg.Text('INVOICE HEADER DETAILS')],
+    [sg.Text('Invoice Date', size =(15, 1)), sg.Input(key='-INVOICE_DATE-', default_text=f"{datetime.datetime.now():%d-%m-%Y}",do_not_clear=False),
+     sg.Text('DD-MM-YYYY', size=(15,1))],
+    [sg.Text('Invoice number', size =(15, 1)), sg.Input(key='-INVOICE_NUMBER-', do_not_clear=False)],
+    [sg.Text('PO number', size=(15, 1)), sg.Input(key='-PO-', do_not_clear=False)],
+    [sg.Text('Coding Sring', size=(15, 1)), sg.Input(key='-CODING_STRING-', do_not_clear=False)],
+    [sg.Text('Created by', size=(15, 1)), sg.Input(key='-CREATOR-')],
+    [sg.Text('Use Tax?', size=(15,1)), sg.Radio('Yes', 2, enable_events=True, default=True, key='-YES_TAX-'), sg.Radio('No',2, enable_events=True, key='-NO_TAX-')],
+    [sg.Text(' ')],
+    [sg.Text('ONLY FILL IF CREDIT NOTA')],
+    [sg.Text('Original Invoice number', size=(15, 1)), sg.Input(key='-ORIGINAL_INVOICE-')],
+    [sg.Text('Reason Credit nota', size=(15, 1)), sg.Input(key='-REASON-')],
     [sg.Text(' ')],
     [sg.Text('FACTUUR LINE ITEMS')],
-    [sg.Text('Product', size=(15, 1)), sg.Input(key='-PRODUCT_DESCRIPTION-'), sg.Text('Aantal', size=(5, 1)), sg.Input(key='-COUNT-'),
-    sg.Text('Stuksprijs', size=(10, 1)), sg.Input(key='-ITEM_PRICE-'), sg.Text('BTW %', size=(5, 1)), sg.Input(key='-TAX-')],
+    [sg.Text('Product', size=(15, 1)), sg.Input(key='-PRODUCT_DESCRIPTION-'), sg.Text('# Items', size=(5, 1)), sg.Input(size=(5, 1), key='-COUNT-'),
+    sg.Text('Item price', size=(10, 1)), sg.Input(size=(5, 1),key='-ITEM_PRICE-'), sg.Text('VAT %', size=(5, 1)), sg.Input(size=(5,1), key='-TAX-')],
+    [sg.Text('Currency', size=(15,1)), sg.Input(key='-CURRENCY-', default_text="€")],
     [sg.Text(' ')],
-    [sg.Text('BESTANDSNAAM & FOLDERLOCATIE')],
-    [sg.Text('Bestandsnaam', size=(15, 1)), sg.Input(key='-FILENAME-', do_not_clear=False)],
-    [sg.Text("Choose a file: "), sg.Input(key='-FILEFOLDER-'), sg.FolderBrowse()],
+    [sg.Text('FILE NAME & FOLDER LOCATIION')],
+    [sg.Text('File Name', size=(15, 1)), sg.Input(key='-FILENAME-', do_not_clear=False)],
+    [sg.Text("Choose a folder: "), sg.Input(key='-FILEFOLDER-'), sg.FolderBrowse()],
     [sg.Button('SUBMIT'), sg.Button('CANCEL')]
 ]
 
@@ -66,6 +76,7 @@ while True:
         v13 = values['-SUPPLIER_BANKACCOUNT-']
         v14 = values['-INVOICE_NUMBER-']
         v15 = values['-PO-']
+        v30 = values['-CODING_STRING-']
         v16 = values['-PRODUCT_DESCRIPTION-']
         v17 = values['-COUNT-']
         v18 = values['-ITEM_PRICE-']
@@ -73,11 +84,29 @@ while True:
         v20 = values['-CREATOR-']
         v21 = values['-FILENAME-']
         v22 = values['-FILEFOLDER-']
+        v26 = values['-CURRENCY-']
+        v28 = values['-ORIGINAL_INVOICE-']
+        v29 = values['-REASON-']
+
+        #condition to determine invoice type
         if values['-NORMAL_INVOICE-'] == True:
             v23 = 'Normal'
         elif values['-CREDIT_NOTA-'] == True:
             v23 = 'Credit'
-        generate_invoice(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v13, v12, v17, v18, v16, v19, v14, v15, v20, v21, v22, v23)
+
+        #condition to determine invoice date
+        if values['-INVOICE_DATE-'] == "":
+            v25 = values['-INVOICE_DATE-']
+        else:
+            v25 = datetime.datetime.strptime(values['-INVOICE_DATE-'], "%d-%m-%Y")
+
+        #conition to determine to use tax or not
+        if values['-YES_TAX-'] == True:
+            v27 = True
+        elif values['-NO_TAX-'] == True:
+            v27 = False
+
+        generate_invoice(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v13, v12, v17, v18, v16, v19, v14, v15, v30, v20, v21, v22, v23, v25, v26, v27, v28, v29)
         #window.close()
 
 window.close()
